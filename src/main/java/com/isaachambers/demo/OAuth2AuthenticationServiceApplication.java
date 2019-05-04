bc9e07e1bc65328025accd3b21fc4178749d3a99
isaachambers <isaaceduardk@gmail.com>
2019-05-04 13:42:31 +0300

    Setting Up Project

---
+++ src/main/java/com/isaachambers/demo/OAuth2AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.isaachambers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthenticationServiceApplication.class, args);
+	}
+
+}
