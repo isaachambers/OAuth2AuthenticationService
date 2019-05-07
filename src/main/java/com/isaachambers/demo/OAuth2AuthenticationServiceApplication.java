4f0cfcca3b26997c807a76218fbcf91e5a9a4f0b
isaachambers <isaaceduardk@gmail.com>
2019-05-08 00:31:44 +0300

    Adding OAuth Configuration

--- src/main/java/com/isaachambers/demo/OAuth2AuthenticationServiceApplication.java
+++ src/main/java/com/isaachambers/demo/OAuth2AuthenticationServiceApplication.java
@@ -3,11 +3,13 @@ package com.isaachambers.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableResourceServer
+@EnableAuthorizationServer
 public class OAuth2AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/isaachambers/demo/controller/AuthController.java
+++ src/main/java/com/isaachambers/demo/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/auth")
 public class AuthController {
 
 	@RequestMapping(value = { "/user" }, produces = "application/json")
